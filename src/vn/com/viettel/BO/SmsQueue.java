ece2be1171c0d081fe20a16b23e33f7443f55f95
Nguyen Van Dai <nguyenvandaibe@gmail.com>
2021-07-13 18:02:58 +0700

    Cập nhật SmsQueue.

---
+++ src/vn/com/viettel/BO/SmsQueue.java
@@ -0,0 +1,320 @@
+package vn.com.viettel.BO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class SmsQueue {
+
+    private String TenantId;
+    private String Mobile;
+    private int SmsTypeId;
+    private String ReceiverId;
+    private String SchoolYearId;
+    private String SchoolYearCode;
+    private String SmsContent;
+    private String Username;
+    private String SenderName;
+    private String ReceiverName;
+    private int SmsCnt;
+    private int ReceiveType;
+    private String SmsSubscriberId;
+    private String SenderUnitId;
+    private String ReceiverUnitName;
+    private boolean IsAuto;
+    private Long SenderGroup;
+    private String ServiceId;
+    private int ContentType;
+    private boolean IsOnSchedule;
+    private String SmsTimerConfigId;
+    private boolean IsAdmin;
+    private String SenderId;
+    private String ContactGroupId;
+    private boolean IsCustomType;
+    private int TelcoId;
+    private Date SyncTime;
+    private int RetryNum;
+    private int LastDigitNumber;
+    private int YearId;
+
+    public SmsQueue() {
+    }
+
+    public SmsQueue(ResultSet rs) throws SQLException {
+        TenantId = rs.getString("TenantId");
+        Mobile = rs.getString("Mobile");
+        SmsTypeId = rs.getInt("SmsTypeId");
+        ReceiverId = rs.getString("ReceiverId");
+        SchoolYearId = rs.getString("SchoolYearId");
+        SchoolYearCode = rs.getString("SchoolYearCode");
+        SmsContent = rs.getString("SmsContent");
+        Username = rs.getString("Username");
+        SenderName = rs.getString("SenderName");
+        ReceiverName = rs.getString("ReceiverName");
+        SmsCnt = rs.getInt("SmsCnt");
+        ReceiveType = rs.getInt("ReceiveType");
+        SmsSubscriberId = rs.getString("SmsSubscriberId");
+        SenderUnitId = rs.getString("SenderUnitId");
+        ReceiverUnitName = rs.getString("ReceiverUnitName");
+        IsAuto = rs.getBoolean("IsAuto");
+        SenderGroup = rs.getLong("SenderGroup");
+        ServiceId = rs.getString("ServiceId");
+        ContentType = rs.getInt("ContentType");
+        IsOnSchedule = rs.getBoolean("IsOnSchedule");
+        SmsTimerConfigId = rs.getString("SmsTimerConfigId");
+        IsAdmin = rs.getBoolean("IsAdmin");
+        SenderId = rs.getString("SenderId");
+        ContactGroupId = rs.getString("ContactGroupId");
+        IsCustomType = rs.getBoolean("IsCustomType");
+        TelcoId = rs.getInt("TelcoId");
+        SyncTime = rs.getTime("SyncTime");
+        RetryNum = rs.getInt("RetryNum");
+        LastDigitNumber = rs.getInt("LastDigitNumber");
+        YearId = rs.getInt("YearId");
+    }
+
+    public String getTenantId() {
+        return TenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        TenantId = tenantId;
+    }
+
+    public String getMobile() {
+        return Mobile;
+    }
+
+    public void setMobile(String mobile) {
+        Mobile = mobile;
+    }
+
+    public int getSmsTypeId() {
+        return SmsTypeId;
+    }
+
+    public void setSmsTypeId(int smsTypeId) {
+        SmsTypeId = smsTypeId;
+    }
+
+    public String getReceiverId() {
+        return ReceiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        ReceiverId = receiverId;
+    }
+
+    public String getSchoolYearId() {
+        return SchoolYearId;
+    }
+
+    public void setSchoolYearId(String schoolYearId) {
+        SchoolYearId = schoolYearId;
+    }
+
+    public String getSchoolYearCode() {
+        return SchoolYearCode;
+    }
+
+    public void setSchoolYearCode(String schoolYearCode) {
+        SchoolYearCode = schoolYearCode;
+    }
+
+    public String getSmsContent() {
+        return SmsContent;
+    }
+
+    public void setSmsContent(String smsContent) {
+        SmsContent = smsContent;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getSenderName() {
+        return SenderName;
+    }
+
+    public void setSenderName(String senderName) {
+        SenderName = senderName;
+    }
+
+    public String getReceiverName() {
+        return ReceiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        ReceiverName = receiverName;
+    }
+
+    public int getSmsCnt() {
+        return SmsCnt;
+    }
+
+    public void setSmsCnt(int smsCnt) {
+        SmsCnt = smsCnt;
+    }
+
+    public int getReceiveType() {
+        return ReceiveType;
+    }
+
+    public void setReceiveType(int receiveType) {
+        ReceiveType = receiveType;
+    }
+
+    public String getSmsSubscriberId() {
+        return SmsSubscriberId;
+    }
+
+    public void setSmsSubscriberId(String smsSubscriberId) {
+        SmsSubscriberId = smsSubscriberId;
+    }
+
+    public String getSenderUnitId() {
+        return SenderUnitId;
+    }
+
+    public void setSenderUnitId(String senderUnitId) {
+        SenderUnitId = senderUnitId;
+    }
+
+    public String getReceiverUnitName() {
+        return ReceiverUnitName;
+    }
+
+    public void setReceiverUnitName(String receiverUnitName) {
+        ReceiverUnitName = receiverUnitName;
+    }
+
+    public boolean isAuto() {
+        return IsAuto;
+    }
+
+    public void setAuto(boolean auto) {
+        IsAuto = auto;
+    }
+
+    public Long getSenderGroup() {
+        return SenderGroup;
+    }
+
+    public void setSenderGroup(Long senderGroup) {
+        SenderGroup = senderGroup;
+    }
+
+    public String getServiceId() {
+        return ServiceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        ServiceId = serviceId;
+    }
+
+    public int getContentType() {
+        return ContentType;
+    }
+
+    public void setContentType(int contentType) {
+        ContentType = contentType;
+    }
+
+    public boolean isOnSchedule() {
+        return IsOnSchedule;
+    }
+
+    public void setOnSchedule(boolean onSchedule) {
+        IsOnSchedule = onSchedule;
+    }
+
+    public String getSmsTimerConfigId() {
+        return SmsTimerConfigId;
+    }
+
+    public void setSmsTimerConfigId(String smsTimerConfigId) {
+        SmsTimerConfigId = smsTimerConfigId;
+    }
+
+    public boolean isAdmin() {
+        return IsAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        IsAdmin = admin;
+    }
+
+    public String getSenderId() {
+        return SenderId;
+    }
+
+    public void setSenderId(String senderId) {
+        SenderId = senderId;
+    }
+
+    public String getContactGroupId() {
+        return ContactGroupId;
+    }
+
+    public void setContactGroupId(String contactGroupId) {
+        ContactGroupId = contactGroupId;
+    }
+
+    public boolean isCustomType() {
+        return IsCustomType;
+    }
+
+    public void setCustomType(boolean customType) {
+        IsCustomType = customType;
+    }
+
+    public int getTelcoId() {
+        return TelcoId;
+    }
+
+    public void setTelcoId(int telcoId) {
+        TelcoId = telcoId;
+    }
+
+    public Date getSyncTime() {
+        return SyncTime;
+    }
+
+    public void setSyncTime(Date syncTime) {
+        SyncTime = syncTime;
+    }
+
+    public int getRetryNum() {
+        return RetryNum;
+    }
+
+    public void setRetryNum(int retryNum) {
+        RetryNum = retryNum;
+    }
+
+    public int getLastDigitNumber() {
+        return LastDigitNumber;
+    }
+
+    public void setLastDigitNumber(int lastDigitNumber) {
+        LastDigitNumber = lastDigitNumber;
+    }
+
+    public int getYearId() {
+        return YearId;
+    }
+
+    public void setYearId(int yearId) {
+        YearId = yearId;
+    }
+
+    public String getPara() {
+        String para = "TenantId=" + TenantId;
+        return para;
+    }
+}
--- src/vn/com/viettel/smas/BulkSMSApplication.java
+++ src/vn/com/viettel/smas/BulkSMSApplication.java
@@ -5,34 +5,26 @@
  */
 package vn.com.viettel.smas;
 
-import vn.com.viettel.util.Parameters;
-import java.rmi.RemoteException;
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.util.Date;
-import java.util.List;
-
 import org.apache.log4j.Logger;
-import vn.com.viettel.BO.SmsMT;
+import org.apache.log4j.Priority;
+import vn.com.viettel.BO.SmsQueue;
 import vn.com.viettel.DAL.MTDAL;
-import vn.com.viettel.DAL.PreNumberTelcoDAL;
 import vn.com.viettel.DAL.TimerDAL;
 import vn.com.viettel.DAL.UtilBusnisness;
 import vn.com.viettel.bulk.SmsBrandNameWs;
-
 import vn.com.viettel.bulk.SmsBulkNonUnicodeWs;
 import vn.com.viettel.dataSource.ConnectionPoolManager;
-import vn.com.viettel.util.StringUtil;
 import vn.com.viettel.thread.KThreadPoolExecutor;
-import vn.com.viettel.util.CommonUtils;
-import vn.com.viettel.util.GlobalConstant;
-import vn.com.viettel.util.LogUtil;
+import vn.com.viettel.util.*;
 import ws.bulkSms.impl.Result;
 import ws.bulkSms.impl.ResultBO;
+
+import java.rmi.RemoteException;
+import java.sql.*;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 public class BulkSMSApplication {
 
@@ -259,29 +251,29 @@ public class BulkSMSApplication {
 
                 //connection.setAutoCommit(false);
                 //lay danh sach tin nhan khong dau
-                List<SmsMT> lstSMS = MTDAL.getSmsMt(connection, statement, rs, node, GlobalConstant.SEND_NORMAL, log);
+                List<SmsQueue> lstSMS = MTDAL.getSmsQueue(connection, statement, rs, node, GlobalConstant.SEND_NORMAL, log);
                 if (lstSMS == null || lstSMS.isEmpty()) {
                     //LogUtil.writeInfoLog(logger, "Khong co du lieu gui tin");
                     LogUtil.InfoExt(log, GlobalConstant.LOG_TYPE_INFO, CLASS_NAME, "doWorkSchool", CommonUtils.getDateNow(), "Node=" + node, "Khong co du lieu");
                     return;
                 }
 
-                MTDAL.UpdateStatusSMSSending(connection, lstSMS, log);
+//                MTDAL.UpdateStatusSMSSending(connection, lstSMS, log);
                 //this.processSendSMS(lstSMS);
                 SendNotification sendNotification = new SendNotification();
-                for (SmsMT objMt : lstSMS) {
+                for (SmsQueue queueItem : lstSMS) {
                     //LogUtil.writeInfoLog(logger, log, "Gui tin den so: " + objMt.getUSER_ID());
-                    processSendSMS(objMt);
+                    processSendSMS(queueItem);
 
-                    if (!"".equals(objMt.getDEVICE_TOKEN()) && objMt.getDEVICE_TOKEN() != null) {
-                        try {
-                            log.info("Send notification");
-                            sendNotification.processSendNotification(objMt);
-                        } catch (Exception ex) {
-                            logMM.error(ex.getMessage(), ex);
-                        }
-
-                    }
+//                    if (!"".equals(queueItem.getDEVICE_TOKEN()) && objMt.getDEVICE_TOKEN() != null) {
+//                        try {
+//                            log.info("Send notification");
+//                            sendNotification.processSendNotification(objMt);
+//                        } catch (Exception ex) {
+//                            logMM.error(ex.getMessage(), ex);
+//                        }
+//
+//                    }
 
                 }
             } catch (SQLException | NumberFormatException e) {
@@ -303,12 +295,12 @@ public class BulkSMSApplication {
             }
         }
 
-        void processSendSMS(final SmsMT objSms) {
+        void processSendSMS(final SmsQueue queueItem) {
             KThreadPoolExecutor.execute(new Runnable() {
                 @Override
                 public void run() {
 
-                    String para = objSms.getPara();
+                    String para = queueItem.getPara();
                     Date eventDate = CommonUtils.getDateNow();
                     //Connection connectionThread = null;
                     //CallableStatement stmt = null;
@@ -319,54 +311,56 @@ public class BulkSMSApplication {
                     Boolean result = false;
                     //LogUtil.writeInfoLog(logger, log, "Bat dau gui tin nhan den so: " + objSms.getUSER_ID());
                     // Xoa ky tu dac biet va ky tu unicode
-                    String sms = StringUtil.removeSpecialChar(objSms.getSMS_CONTENT().trim());
+                    String sms = StringUtil.removeSpecialChar(queueItem.getSmsContent().trim());
                     //String phone = objSms.getUSER_ID();
                     StringBuilder strphone = new StringBuilder(String.valueOf(Parameters.CountryCode));
-                    strphone.append(UtilBusnisness.getISDN(objSms.getUSER_ID()));
+                    strphone.append(UtilBusnisness.getISDN(queueItem.getReceiverId()));
                     String phone = strphone.toString().trim();
 
-                    String historyID = objSms.getHISTORY_RAW_ID();
-                    int isNumberRouting = objSms.getIS_NUMBER_ROUTING();
-                    String currOperator = objSms.getCURR_OPERATOR();
-                    int isHome = objSms.getIS_HOME();
-
-                    if (PreNumberTelcoDAL.isVinaPhoneNumber(phone, isNumberRouting, currOperator)) {
-                        // Cat ky tu xuong dong (newline) doi voi dau so Vina
-                        sms = StringUtil.removeNewlineChar(sms);
-                        //Lay brandName
-                        if (!"".equals(objSms.getSERVICE_ID()) && objSms.getSERVICE_ID() != null) {
-                            serviceID = objSms.getSERVICE_ID();
-                        } else {
-                            serviceID = Parameters.SERVICE_ID_OTHER;
-                        }
-                    } else if (PreNumberTelcoDAL.isViettelPhoneNumber(phone, isNumberRouting, isHome)) {
-                        maxLength = Parameters.SMS_P1_MAXLENGTH_VIETTEL;
-
-                        //Lay brandName
-                        if (!"".equals(objSms.getSERVICE_ID()) && objSms.getSERVICE_ID() != null) {
-                            serviceID = objSms.getSERVICE_ID();
-                        } else {
-                            serviceID = Parameters.SERVICE_ID_VIETTEL;
-                        }
-                    } else//Mang mobi, VNmobi...
-                    if (!"".equals(objSms.getSERVICE_ID()) && objSms.getSERVICE_ID() != null) {
-                        serviceID = objSms.getSERVICE_ID();
-                    } else {
-                        serviceID = Parameters.SERVICE_ID_OTHER;
-                    }
-
+//                    String historyID = objSms.getHISTORY_RAW_ID();
+//                    int isNumberRouting = objSms.getIS_NUMBER_ROUTING();
+//                    String currOperator = objSms.getCURR_OPERATOR();
+//                    int isHome = objSms.getIS_HOME();
+//
+//                    if (PreNumberTelcoDAL.isVinaPhoneNumber(phone, isNumberRouting, currOperator)) {
+//                        // Cat ky tu xuong dong (newline) doi voi dau so Vina
+//                        sms = StringUtil.removeNewlineChar(sms);
+//                        //Lay brandName
+//                        if (!"".equals(objSms.getSERVICE_ID()) && objSms.getSERVICE_ID() != null) {
+//                            serviceID = objSms.getSERVICE_ID();
+//                        } else {
+//                            serviceID = Parameters.SERVICE_ID_OTHER;
+//                        }
+//                    } else if (PreNumberTelcoDAL.isViettelPhoneNumber(phone, isNumberRouting, isHome)) {
+//                        maxLength = Parameters.SMS_P1_MAXLENGTH_VIETTEL;
+//
+//                        //Lay brandName
+//                        if (!"".equals(objSms.getSERVICE_ID()) && objSms.getSERVICE_ID() != null) {
+//                            serviceID = objSms.getSERVICE_ID();
+//                        } else {
+//                            serviceID = Parameters.SERVICE_ID_VIETTEL;
+//                        }
+//                    } else//Mang mobi, VNmobi...
+//                    if (!"".equals(queueItem.getServiceId()) && queueItem.getServiceId() != null) {
+//                        serviceID = queueItem.getServiceId();
+//                    } else {
+//                        serviceID = Parameters.SERVICE_ID_OTHER;
+//                    }
+                    serviceID = queueItem.getServiceId();
                     try {
 
                         // Thuc hien gui tin nhan
                         LogUtil.InfoExt(log, GlobalConstant.LOG_TYPE_END_ACTION, CLASS_NAME, "processSendSMS", eventDate, para, "start send sms");
                         //Random random = new Random();
                         //result = random.nextBoolean(); 
-                        result = sendSMSProcess(serviceID, maxLength, sms, phone, historyID);
+                        result = sendSMSProcess(serviceID, maxLength, sms, phone, UUID.randomUUID().toString());
                         LogUtil.InfoExt(log, GlobalConstant.LOG_TYPE_END_ACTION, CLASS_NAME, "processSendSMS", eventDate, para, "end send sms, result=" + result);
                         //Cap nhat trang thai tin nhan trong history
-                        MTDAL.UpdateSMSHistory(objSms, result, serviceID, log);
+//                        MTDAL.UpdateSMSHistory(queueItem, result, serviceID, log);
+                        logMM.log(Priority.toPriority(Priority.INFO_INT), "Cap nhat trang thai tin nhan trong history");
 
-                    } catch (SQLException e) {
+                        LogUtil.InfoExt(log, "INFO", CLASS_NAME, "processSendSMS", CommonUtils.getDateNow(), para, "OK");
+                    } catch (Exception e) {
                         LogUtil.ErrorExt(log, CLASS_NAME, "processSendSMS", CommonUtils.getDateNow(), para, e);
                     }
 
